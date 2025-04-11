7cef794d248156582ad029c7f24863a8c012f61c
n6v26r <dedu.razvan.matei@gmail.com>
2025-04-11 16:50:09 +0300

    redo code truncation

--- cmd/submission/details.go
+++ cmd/submission/details.go
@@ -132,9 +132,9 @@ func printTemplateSubmission(details SubmissionDetails, formattedTime string, co
 }
 
 func formatCodeOutput(code string, lang string) string {
-	// if len(code) > 500 {
-	// 	code = code[:500] + "...\n"
-	// }
+	if len(code) > 600 {
+		code = code[:600] + "...\n"
+	}
 
 	// More syntax files: https://github.com/zyedidia/highlight
 	syntaxFile, err := highlightDir.ReadFile("highlight/" + lang + ".yaml")
